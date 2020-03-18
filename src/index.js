4d434037ed30591c68b5d05699b84135e0985fcf
NAMartynovich <nike0@bk.ru>
2020-03-18 21:50:07 +0300

    Компоненты

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  )
+}
+
+const AppHeader = () => {
+  return (
+    <h1>Мой список дел</h1>
+  )
+}
+
+const SearchPanel = () => {
+  return (
+    <input placeholder='поиск' />
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
