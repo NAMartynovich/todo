a89a9844f0ae73c1b31fdd7fee9dab8273ea3691
NAMartynovich <nike0@bk.ru>
2020-03-20 18:48:03 +0300

    структура реакт прокта

---
+++ src/components/app/app.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import AppHeader from '../app-header'
+import SearchPanel from '../search-panel'
+import TodoList from '../todo-list'
+import ItemStatusFilter from '../item-status-filter'
+import './app.css'
+
+const App = () => {
+  const todoData = [
+    { label: 'Выпить кофе', important: false, id: 1 },
+    { label: 'Создать приложение', important: true, id: 2 },
+    { label: 'Пообедать', important: false, id: 3 },
+  ]
+  return (
+    <div className='container app' >
+      <AppHeader />
+      <SearchPanel />
+      <ItemStatusFilter />
+      <TodoList todos={todoData} />
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AppHeader from './components/app-header'
-import SearchPanel from './components/search-panel'
-import TodoList from './components/todo-list'
-import ItemStatusFilter from './components/item-status-filter'
-
-const App = () => {
-  const todoData = [
-    { label: 'Выпить кофе', important: false, id: 1 },
-    { label: 'Создать приложение', important: true, id: 2 },
-    { label: 'Пообедать', important: false, id: 3 },
-  ]
-  return (
-    <div className='container' >
-      <AppHeader />
-      <SearchPanel />
-      <ItemStatusFilter />
-      <TodoList todos={todoData} />
-    </div>
-  )
-}
-
+import App from './components/app'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
