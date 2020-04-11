2cca1aef75821d5d3e9a7ff77058fb633f52626a
NAMartynovich <nike0@bk.ru>
2020-04-11 12:28:15 +0300

    отправка из формы

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react'
 
 import './item-add-form.css'
 
+
 export default class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  }
+
+  onLabelChage = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.onItemAdded(this.state.label)
+    this.setState({
+      label: ''
+    })
+  }
 
   render() {
     return (
-      <div className='item-add-form'>
+      <form className='item-add-form d-flex'
+        onSubmit={this.onSubmit}>
+        <input type='text'
+          className='form-control'
+          onChange={this.onLabelChage}
+          placeholder='Что должно быть сделано'
+          value={this.state.label} />
         <button
-          className='btn btn-outline-secondary'
-          onClick={() => this.props.onItemAdded('Привет мир!')}>
-          Add Item
+          className='btn btn-outline-secondary'>
+          Add
         </button>
-      </div>
+      </form>
     )
   }
 }
